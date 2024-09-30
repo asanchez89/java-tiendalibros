8c884e2ddf5c97ebe4924ca4fafdd429c772bc07
asanchez89 <asanchez@deltare.net>
2024-09-30 13:16:37 -0400

    Se incluye los componentes del formulario

--- src/main/java/com/curso/libros/vista/LibroForm.java
+++ src/main/java/com/curso/libros/vista/LibroForm.java
@@ -7,17 +7,28 @@ import org.springframework.stereotype.Component;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+
 @Component
 public class LibroForm extends JFrame {
     LibroServicio libroServicio;
     private JPanel panel;
     private JTable tablaLibros;
+    private JTextField libroTexto;
+    private JTextField autorTexto;
+    private JTextField precioTexto;
+    private JTextField existenciasTexto;
+    private JButton agregarButton;
+    private JButton modificarButton;
+    private JButton eliminarButton;
     private DefaultTableModel tablaModeloLibros;
 
     @Autowired
     public LibroForm(LibroServicio libroServicio){
         this.libroServicio = libroServicio;
         iniciarForma();
+        agregarButton.addActionListener(e -> {
+
+        });
     }
 
     private void iniciarForma() {
@@ -38,5 +49,21 @@ public class LibroForm extends JFrame {
         String[] cabeceras = {"Id", "Libro", "Autor", "Precio", "Existencias"};
         this.tablaModeloLibros.setColumnIdentifiers(cabeceras);
         this.tablaLibros = new JTable(tablaModeloLibros);
+        listarLibros();
+    }
+
+    private void listarLibros(){
+        tablaModeloLibros.setRowCount(0);
+        var libros = libroServicio.listarLibros();
+        libros.forEach((libro)->{
+            Object[] renglonLibro = {
+                    libro.getIdLibro(),
+                    libro.getNombreLibro(),
+                    libro.getAutor(),
+                    libro.getPrecio(),
+                    libro.getExistencias()
+            };
+            tablaModeloLibros.addRow(renglonLibro);
+        });
     }
 }
